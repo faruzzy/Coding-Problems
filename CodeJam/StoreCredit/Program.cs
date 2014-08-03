4752fce23b225d66e4f9662bfb44a23590e90fec
Roland Pangu <faruzzy@gmail.com>
2014-08-02 21:36:11 -0700

    Added Google CodeJam problems resolutions

---
+++ CodeJam/StoreCredit/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreCredit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("small.in");
+            int n = Convert.ToInt16(lines.First());
+            List<Dictionary<int, int>> list = new List<Dictionary<int, int>>();
+
+            for (int i = 1; i < n; i++)
+            {
+                int max = Convert.ToInt16(lines[i]);
+                int x = Convert.ToInt16(lines[i + 1]);
+                int[] prices = Array.ConvertAll(lines[i + 2].Split(' '), int.Parse);
+
+                foreach (int price in prices)
+                {
+                    Dictionary<int, int> set = new Dictionary<int,int>();
+                    int remainer = max - price;
+                    int idx = Array.IndexOf(prices, remainer);
+
+                    if (idx != -1)
+                    {
+                        set.Add(price, prices[idx]);
+                        list.Add(set);
+                        break;
+                    }
+                }
+                i += 2;
+            }
+
+            using(StreamWriter sw = new StreamWriter("output.txt"))
+            {
+                for (int i = 0; i < list.Count(); i++)
+                    foreach (var dictionary in list[i])
+                        sw.WriteLine("Case #" + (i + 1) + ":" + dictionary.Key + " " + dictionary.Value);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Coding-Problems
 ===============
+This is a personal collection of Algorithms puzzle that I collected and attempt to resolve as I come along them.
