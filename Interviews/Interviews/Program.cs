f8eb884c73959fc7662b96b4d07bb8ac007ec6e0
Roland Pangu <faruzzy@gmail.com>
2014-08-04 10:59:16 -0700

    Created Interviews folder

---
+++ Interviews/Interviews/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interviews
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter letters: ");
+            string letters = Console.ReadLine();
+
+            Console.WriteLine();
+
+            bool a = hasDuplicate(letters.ToCharArray());
+            if (a)
+                Console.WriteLine("duplicate letters were found");
+            else
+                Console.WriteLine("no duplicate letters were found");
+
+            Console.ReadLine();
+        }
+
+        static bool hasDuplicate(char[] letters)
+        {
+            bool hasDuplicate = false;
+            List<char> c = new List<char>();
+
+            for (int i = 0; i < letters.Length; i++ )
+            {
+                if (i == 0)
+                {
+                    c.Add(letters[i]);
+                    continue;
+                }
+
+                if (c.IndexOf(letters[i]) == -1)
+                    c.Add(letters[i]);
+                else
+                    return true;
+            }
+
+            return hasDuplicate;
+        }
+
+    }
+}
---
+++ Interviews/Interviews/README.md
@@ -0,0 +1,1 @@
+﻿Write a function that takes a collection of letters and determines if there are any duplicate letters
