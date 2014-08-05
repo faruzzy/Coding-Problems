8715837584f8658d686aecfb55a31e1657346d7f
Roland Pangu <faruzzy@gmail.com>
2014-08-05 12:10:22 -0700

    Added Implimentation of String.Split() method

---
+++ Interviews/SplitFunction/ExtensionMethods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplitFunction
+{
+    public static class ExtensionMethods
+    {
+        public static string[] MySplit(this string str, char pattern)
+        {
+            List<string> words = new List<string>();
+
+            while (str.Length > 0)
+            {
+                int index = str.IndexOf(' ');
+                if (index == -1)
+                {
+                    words.Add(str.Substring(0, str.Length));
+                    str = str.Substring(str.Length);
+                }
+                else
+                {
+                    words.Add(str.Substring(0, index));
+                }
+
+                str = str.Substring(index + 1);
+            }
+
+            return words.ToArray();
+        }
+    }
+}
---
+++ Interviews/SplitFunction/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplitFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = "Roland Pangu is a programmer".MySplit(' ');
+            foreach (string w in words)
+                Console.WriteLine(w);
+
+            Console.ReadLine();
+        }
+
+        static string[] Split(string str)
+        {
+            List<string> words = new List<string>();
+
+            while(str.Length > 0)
+            {
+                int index = str.IndexOf(' ');
+                if (index == -1)
+                {
+                    words.Add(str.Substring(0, str.Length));
+                    str = str.Substring(str.Length);
+                }
+                else
+                {
+                    words.Add(str.Substring(0, index));
+                }
+
+                str = str.Substring(index + 1);
+            }
+
+            return words.ToArray();
+        }
+    }
+}
