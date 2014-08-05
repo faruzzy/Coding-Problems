f89a7c0e2510e52551c9f787231eb19b0171dac1
Roland Pangu <faruzzy@gmail.com>
2014-08-05 11:29:22 -0700

    Fixed typo

--- javascript/call.js
+++ javascript/call.js
@@ -1,6 +1,6 @@
 Function.prototype.myCall = function(scope) {
 	'use strict';
-	var args = Array.slice.call(arguments, 1);
+	var args = Array.prototype.slice.call(arguments, 1);
 	var fn = this;
 	return function() {
 		return fn.apply(scope, args);
