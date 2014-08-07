6415435ace5c0806de0982945a18afd04a8586ee
Roland Pangu <faruzzy@gmail.com>
2014-08-06 23:11:14 -0700

    Added complete solution of Reverse with no Split()

--- CodeJam/ReverseWords_No_Split/Program.cs
+++ CodeJam/ReverseWords_No_Split/Program.cs
@@ -12,24 +12,43 @@ namespace ReverseWords_No_Split
         static void Main(string[] args)
         {
             string[] lines = File.ReadAllLines("small.in");
-            List<string> words = new List<string>();
+            List<string> ls = new List<string>();
+
             for(int j = 1; j < lines.Length; j++)
             {
+                List<string> words = new List<string>();
                 string line = lines[j];
-                int idx = 0;
+                int start = 0;
                 while (line.Length > 0)
                 {
-                    idx = line.IndexOf(' ');
+                    if (line.IndexOf(' ') != -1)
+                    {
+                        int idx = line.IndexOf(' ');
+                        words.Add(line.Substring(start, idx));
+                        line = line.Substring(idx + 1);
+                    }
+                    else
+                    {
+                        words.Add(line.Substring(0, line.Length));
+                        line = line.Substring(line.Length);
+                    }
                 }
-                /*string line = lines[j];
-                for(int i = 0; i < line.Length; i++)
-                {
-                    int idx = line.IndexOf(' ');
-                    words.Add(line.Substring(i, idx));
-                    i = idx;
-                }*/
+
+                StringBuilder sb = new StringBuilder();
+                
+                for(int i = words.Count - 1; i >= 0; i--)
+                    sb.Append(words[i] + " ");
+
+                ls.Add(sb.ToString());
             }
 
+            using (StreamWriter sw = new StreamWriter(File.Open("ouput.txt", FileMode.Create)))
+            {
+                for (int i = 0; i < ls.Count(); i++)
+                {
+                    sw.WriteLine(ls[i]);
+                }
+            }
         }
     }
 }
