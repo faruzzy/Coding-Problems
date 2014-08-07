a28dd2d9f9604c3f1fcfa891bac6bd6a19bd3955
Roland Pangu <faruzzy@gmail.com>
2014-08-06 22:40:00 -0700

    Added Reversed Words Resolution without Split()

---
+++ CodeJam/ReverseWords_No_Split/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ReverseWords_No_Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("small.in");
+            List<string> words = new List<string>();
+            for(int j = 1; j < lines.Length; j++)
+            {
+                string line = lines[j];
+                int idx = 0;
+                while (line.Length > 0)
+                {
+                    idx = line.IndexOf(' ');
+                }
+                /*string line = lines[j];
+                for(int i = 0; i < line.Length; i++)
+                {
+                    int idx = line.IndexOf(' ');
+                    words.Add(line.Substring(i, idx));
+                    i = idx;
+                }*/
+            }
+
+        }
+    }
+}
