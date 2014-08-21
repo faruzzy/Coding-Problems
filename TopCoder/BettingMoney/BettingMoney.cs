7817ba083e1a595f694578452e0fea83ab0fd72e
Roland Pangu <faruzzy@gmail.com>
2014-08-20 23:04:55 -0700

    Added BettingMoney problem Solution

---
+++ TopCoder/BettingMoney/BettingMoney.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BettingMoney
+{
+    class BettingMoney
+    {
+        static void Main(string[] args)
+        {
+            int sum = moneyMade(new int[] { 100, 100, 100, 100 }, new int[] { 5, 5, 5, 5 }, 0);
+        }
+
+        private static int moneyMade(int[] amounts, int[] centsPerDollar, int finalResult)
+        {
+            int sum = 0;
+            int n = amounts.Length;
+
+            for(int i = 0; i < n; i++)
+            {
+                if (i == finalResult) continue;
+                sum += amounts[i];
+            }
+
+            return (sum * 100) - (centsPerDollar[finalResult] * amounts[finalResult]);
+        }
+    }
+}
