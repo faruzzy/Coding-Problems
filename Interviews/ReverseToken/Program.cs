23c448b29141eed796e547a5a053d41683729f8b
Roland Pangu <faruzzy@gmail.com>
2014-10-06 21:56:51 -0700

    Added Reverse Token Problem solution

---
+++ Interviews/ReverseToken/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseToken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Zamar has been experimenting with ORM";
+            string s = ReverseToken(str);
+            Console.WriteLine(s);
+
+            Console.ReadLine();
+        }
+
+        static string ReverseToken(string str)
+        {
+            string[] tokens = str.Split(' ');
+            var sentence = new StringBuilder();
+
+            foreach(string token in tokens)
+            {
+                var sb = new StringBuilder();
+                for (int i = token.Length - 1; i >= 0; i--)
+                    sb.Append(token[i]);
+
+                sentence.Append(sb.ToString() + " ");
+            }
+
+            string[] stoken = sentence.ToString().Split(' ');
+            var s = new StringBuilder();
+            for (int j = stoken.Length - 1; j >= 0; j--)
+                s.Append(s[j] + " ");
+
+            return s.ToString();
+        }
+    }
+}
