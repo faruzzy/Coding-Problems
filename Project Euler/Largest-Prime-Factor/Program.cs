c82cd5592dddf45aa7033ec71f75e863d214cbf8
Roland Pangu <faruzzy@gmail.com>
2014-10-06 21:57:50 -0700

    Project Euler started

---
+++ Project Euler/Largest-Prime-Factor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest_Prime_Factor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factors = new List<int>();
+            for (int i = 2; i <= 13195; i++)
+            {
+                int c = i;
+                bool check = false;
+                while(c > 1)
+                {
+                    if (c - 1 == 1) break;
+                    c--;
+                    if (i % c == 0)
+                    {
+                        check = true;
+                        break;
+                    }
+                }
+
+                if (!check)
+                {
+                    factors.Add(i);
+                }
+            }
+
+            Console.WriteLine(factors.Max());
+            Console.ReadLine();
+        }
+    }
+}
