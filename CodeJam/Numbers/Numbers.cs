1494d1cc3fa58a4bf07805dcc92fce8b14721db5
Roland Pangu <faruzzy@gmail.com>
2014-10-06 22:01:26 -0700

    Started Numbers problem resolution

---
+++ CodeJam/Numbers/Numbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers
+{
+    class Numbers
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static string calculate(int n)
+        {
+            double c = Math.Pow(3 + Math.Sqrt(5), n);
+            string answer = "" + (int)c;
+
+            if (answer.Length >= 3)
+                return answer.Substring(answer.Length - 3);
+            else
+                return "0" + answer;
+        }
+    }
+}
