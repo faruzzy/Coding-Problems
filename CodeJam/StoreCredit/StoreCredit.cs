bda2ada27817a3184c0c7bea0de822fdc6842602
Roland Pangu <faruzzy@gmail.com>
2014-10-06 21:18:09 -0700

    Logic fix, using a dictionary was an overkill

--- CodeJam/StoreCredit/StoreCredit.cs
+++ CodeJam/StoreCredit/StoreCredit.cs
@@ -13,24 +13,24 @@ namespace StoreCredit
         {
             string[] lines = File.ReadAllLines("small.in");
             int n = Convert.ToInt16(lines.First());
-            List<Dictionary<int, int>> list = new List<Dictionary<int, int>>();
+            var list = new List<int[]>();
 
-            for (int i = 1; i < n; i++)
+            for (int i = 1; i < lines.Length; i++)
             {
                 int max = Convert.ToInt16(lines[i]);
-                int x = Convert.ToInt16(lines[i + 1]);
                 int[] prices = Array.ConvertAll(lines[i + 2].Split(' '), int.Parse);
 
-                foreach (int price in prices)
+                for (int j = 0; j < prices.Length; j++)
                 {
-                    Dictionary<int, int> set = new Dictionary<int, int>();
-                    int remainer = max - price;
+                    var set = new List<int>();
+                    int remainer = max - prices[j];
                     int idx = Array.IndexOf(prices, remainer);
 
                     if (idx != -1)
                     {
-                        set.Add(price, prices[idx]);
-                        list.Add(set);
+                        set.Add(prices[j]);
+                        set.Add(prices[idx]);
+                        list.Add(set.ToArray());
                         break;
                     }
                 }
@@ -40,8 +40,7 @@ namespace StoreCredit
             using(StreamWriter sw = new StreamWriter("output.txt"))
             {
                 for (int i = 0; i < list.Count(); i++)
-                    foreach (var dictionary in list[i])
-                        sw.WriteLine("Case #" + (i + 1) + ":" + dictionary.Key + " " + dictionary.Value);
+                    sw.WriteLine("Case #" + (i + 1) + ": " + list[i][0] + " " + list[i][1]);
             }
         }
     }
