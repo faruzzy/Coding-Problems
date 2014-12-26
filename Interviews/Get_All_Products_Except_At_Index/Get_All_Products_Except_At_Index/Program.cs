9889ad8820018cdc8f6849659184cb6ada6bdac5
Roland Pangu <faruzzy@gmail.com>
2014-12-26 13:22:39 -0700

    get products of all ints except at index
    Write a function that takes an array of integers and returns an array of
    the products.

---
+++ Interviews/Get_All_Products_Except_At_Index/Get_All_Products_Except_At_Index/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Get_All_Products_Except_At_Index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 7, 3, 4 };
+            int[] products = GetProducts(arr);
+
+            foreach (int number in products)
+                Console.WriteLine("result: " + number);
+        }
+
+        public static int[] GetProducts(int[] arr)
+        {
+            int product = 1;
+            int[] ret = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                    if (i == j) continue;
+                    else
+                        product *= arr[j];
+
+                ret[i] = product;
+                product = 1;
+            }
+
+            return ret;
+        }				
+    }
+}
