cf8c059258555c9a3837119d2763f0bbf498984d
Roland Pangu <faruzzy@gmail.com>
2015-01-22 20:51:31 -0700

    Added Insertion Sort algorithm

---
+++ Algorithms/Sorting/InsertionSort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] { 4, 10, 2, 7, 8, 9, 12 };
+            InsertionSort(array);
+
+            foreach (var item in array)
+                Console.Write("{0} ", item);
+
+            Console.ReadKey();
+        }
+
+        static void InsertionSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int j = i;
+                while(j > 0 && array[j - 1] > array[j])
+                {
+                    int temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                    j--;
+                }
+            }
+        }
+    }
+}
