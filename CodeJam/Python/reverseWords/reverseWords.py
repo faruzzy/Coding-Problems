a6b75a533c69c243ae08c30fb71147f570932787
Roland Pangu <faruzzy@gmail.com>
2015-12-19 21:47:18 -0700

    Using unused variable accordingly

--- CodeJam/Python/reverseWords/reverseWords.py
+++ CodeJam/Python/reverseWords/reverseWords.py
@@ -7,8 +7,8 @@ with open('large.in') as f:
     for line in f:
         list.append(' '.join(reversed(line.split())))
 try:
-    os.remove('output.txt')
-    with open('output.txt', 'a') as f:
+    os.remove(output)
+    with open(output, 'a') as f:
         for i in range(0, len(list)):
             f.write('Case #{0}: {1}\n'.format(i+1, list[i]))
 except OSError:
