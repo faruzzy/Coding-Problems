7e174f8c486a1d6665638162a08afc64f3071add
Roland Pangu <faruzzy@gmail.com>
2016-11-04 23:37:17 -0700

    added link to problem

--- DailyProgrammer/Java/Intermediate/IntermediateBlinkingLEDs/IntermediateBlinkingLEDs.java
+++ DailyProgrammer/Java/Intermediate/IntermediateBlinkingLEDs/IntermediateBlinkingLEDs.java
@@ -1,3 +1,5 @@
+// https://www.reddit.com/r/dailyprogrammer/comments/5as91q/20161102_challenge_290_intermediate_blinking_leds/
+
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
