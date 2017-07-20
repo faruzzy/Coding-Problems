287b5b3ff0f5be5765136638dde67b2e3f44f0ca
Roland Pangu <faruzzy@gmail.com>
2017-07-20 10:43:05 -0700

    improved variable names

--- CodeJam/JavaScript/reverseWords/reverseWords.js
+++ CodeJam/JavaScript/reverseWords/reverseWords.js
@@ -1,15 +1,11 @@
 const fs = require('fs');
 
-const [, ...output] = fs.readFileSync('large.in', 'utf8')
-	.trim()
-	.split('\n');
-
-const result = output.map(x => x.split(' ')
-		.reverse()
-		.join(' '));
+const [, ...lines] = fs.readFileSync('large.in', 'utf8').trim().split('\n');
+const output = lines.map(line => line.split(' ').reverse().join(' ')); 
 
 const fd = fs.openSync('result.txt', 'a');
-result.forEach((x, index) => {
+
+output.forEach((x, index) => {
 	fs.writeSync(fd, `Case #${index + 1}: ${x}\n`);
 });
 
